cec85c6714aaeae2cc6a4aab13f316b773c9de9e
Federico Infanti <federico.infanti@sparwelt.de>
2018-08-02 08:19:11 +0000

    Fix failing tests

--- Tests/ImgixTwigExtensionTest.php
+++ Tests/ImgixTwigExtensionTest.php
@@ -43,7 +43,7 @@ class ImgixExtensionTest extends \PHPUnit\Framework\TestCase
         $imgixServiceMock->expects($this->exactly(1))->method('generateUrl')->with('test1.png', ['p1' => 1], ['p2' => 2])->willReturn('generateUrlCalled');
         $imgixServiceMock->expects($this->exactly(1))->method('generateAttributeValue')->with('test2.png', ['p3' => 3], ['p4' => 4])->willReturn('generateAttributeValueCalled');
         $imgixServiceMock->expects($this->exactly(1))->method('generateImage')->with('test3.png', ['p5' => 5], ['p6' => 6])->willReturn('generateImageCalled');
-        $imgixServiceMock->expects($this->exactly(1))->method('convertHtml')->with('html', ['p7' => 7], ['p8' => 8])->willReturn('convertHtmlCalled');
+        $imgixServiceMock->expects($this->exactly(1))->method('transformHtml')->with('html', ['p7' => 7], ['p8' => 8])->willReturn('transformHtmlCalled');
 
         $container = new ContainerBuilder();
         $this->extension->load($configs, $container);
@@ -52,7 +52,7 @@ class ImgixExtensionTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('generateUrlCalled', $container->get(ImgixTwigExtension::class)->generateUrl('test1.png', ['p1' => 1], ['p2' => 2]));
         $this->assertEquals('generateAttributeValueCalled', $container->get(ImgixTwigExtension::class)->generateAttributeValue('test2.png', ['p3' => 3], ['p4' => 4]));
         $this->assertEquals('generateImageCalled', $container->get(ImgixTwigExtension::class)->generateImage('test3.png', ['p5' => 5], ['p6' => 6]));
-        $this->assertEquals('convertHtmlCalled', $container->get(ImgixTwigExtension::class)->transformHtml('html', ['p7' => 7], ['p8' => 8]));
+        $this->assertEquals('transformHtmlCalled', $container->get(ImgixTwigExtension::class)->transformHtml('html', ['p7' => 7], ['p8' => 8]));
 
         $filters = $container->get(ImgixTwigExtension::class)->getFilters();
         $this->assertInstanceOf(TwigFilter::class, $filters[0]);
